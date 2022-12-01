e217a8ced16615485018ff60ab72d324dfe6be0b
Aaron Clements <aaron.clements@smarsh.com>
2022-12-01 14:27:12 -0800

    Dec1

--- terraform/docdb/provision/main.tf
+++ terraform/docdb/provision/main.tf
@@ -13,29 +13,29 @@
 # limitations under the License.
 
 resource "aws_docdb_cluster" "docdb_cluster" {
-  cluster_identifier              = var.cluster_identifier
+  cluster_identifier               = var.cluster_identifier
   db_subnet_group_name            = var.db_subnet_group_name
   db_cluster_parameter_group_name = var.db_cluster_parameter_group_name
   deletion_protection             = var.deletion_protection
   enabled_cloudwatch_logs_exports = var.enabled_cloudwatch_logs_exports
   engine_version                  = var.engine_version
-  final_snapshot_identifier       = var.final_snapshot_identifier
+  final_snapshot_identifier         = var.final_snapshot_identifier
   master_password                 = var.master_password
   master_username                 = var.master_username
   port                            = var.port
   preferred_backup_window         = var.preferred_backup_window
   preferred_maintenance_window    = var.preferred_maintenance_window
-  snapshot_identifier             = var.snapshot_identifier
+  snapshot_identifier              = var.snapshot_identifier
   storage_encrypted               = var.storage_encrypted
   tags                            = var.tags
   vpc_security_group_ids          = var.vpc_security_group_ids
 }
 
 resource "aws_docdb_cluster_instance" "docdb_cluster_instances" {
-  cluster_identifier           = var.cluster_identifier.docdb_cluster.id
-  count                        = var.instances
-  identifier                   = "var.indentifier-${count.index}"
-  instance_class               = var.instance_class
-  preferred_maintenance_window = var.preferred_maintenance_window
-  tags                         = var.tags
+  cluster_identifier               = var.cluster_identifier.docdb_cluster.id
+  count                           = var.instances
+  identifier                       = "var.indentifier-${count.index}"
+  instance_class                  = var.instance_class
+  preferred_maintenance_window    = var.preferred_maintenance_window
+  tags                            = var.tags
 }
